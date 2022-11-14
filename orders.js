f2961fa8e6275ceb4adaee40ccfb51ddef80e4db
Ahmed-Amshoosh <103959192+Ahmed-Amshoosh@users.noreply.github.com>
2022-11-14 19:16:40 +0300

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,49 @@
+const Orders =[
+    {
+        prodcutName:'Foldable Mini Drone',
+        prodcutNumber:'85631',
+        prodcutStatus:'Due',
+        shipping:'Pending',
+    },
+    {
+        prodcutName:'LARVENDER KF102 Drone',
+        prodcutNumber:'36378',
+        prodcutStatus:'Refunded',
+        shipping:'Declined',
+
+    },
+    {
+        prodcutName:'Ruko F11 Pro Drone',
+        prodcutNumber:'49347',
+        prodcutStatus:'Doe',
+        shipping:'DPending',
+    },
+    {
+        prodcutName:'Drone with  Camera Drone',
+        prodcutNumber:'96996',
+        prodcutStatus:'Paid',
+        shipping:'Delivered',
+
+    },
+    {
+        prodcutName:'GPS 4k Drone',
+        prodcutNumber:'22821',
+        prodcutStatus:'Paid',
+        shipping:'Delivered',
+
+    },
+    {
+        prodcutName:'DJI Air 2S',
+        prodcutNumber:'81475',
+        prodcutStatus:'Due',
+        shipping:'Pending',
+
+    },
+    {
+        prodcutName:'Lozenge Drone',
+        prodcutNumber:'00482',
+        prodcutStatus:'Paid',
+        shipping:'Declivered',
+
+    }
+]
